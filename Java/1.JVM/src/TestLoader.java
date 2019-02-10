2d411ad81611e1882e7b3c561ac516f2c1980258
Charan Kumar <charankumar.palla@gmail.com>
2019-02-10 11:55:01 +0530

    ClassLoaders basics

---
+++ Java/1.JVM/src/TestLoader.java
@@ -0,0 +1,18 @@
+
+public class TestLoader {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
+
+
+
+
+
+public class TestLoader {
+	public static void main(String[] args) {
+		System.out.println("test");
+		A a = new A();
+		a.method();
+	}
+}
