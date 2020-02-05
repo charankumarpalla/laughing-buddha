0a39a08865f742807887b7133013f97331055eb3
CharanKumar <charankumar.palla@gmail.com>
2020-02-05 13:32:03 +0530

    Expanded Example

--- PlayGround/Flutter_Dart/Flutter/charan_my_showcase/lib/pages/MyExpanded.dart
+++ PlayGround/Flutter_Dart/Flutter/charan_my_showcase/lib/pages/MyExpanded.dart
@@ -6,6 +6,9 @@ class MyExpanded extends StatefulWidget {
 }
 
 class _MyExpandedState extends State<MyExpanded> {
+  int _flex0 = 1;
+  int _flex1 = 2;
+  int _flex2 = 1;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,7 +27,105 @@ class _MyExpandedState extends State<MyExpanded> {
             ),
           ),
         ),
-        body: Text("In Progress...."),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                  child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: _flex0,
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: GestureDetector(
+                        child: Container(
+                          child: Center(
+                            child: Text(
+                              '$_flex0',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          margin: const EdgeInsets.all(10.0),
+                          color: const Color(0xFF2190f3),
+                          width: 48.0,
+                          height: 48.0,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _flex0++;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: _flex1,
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: GestureDetector(
+                        child: Container(
+                          child: Center(
+                            child: Text(
+                              '$_flex1',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          margin: const EdgeInsets.all(10.0),
+                          color: const Color(0x882196f3),
+                          width: 48.0,
+                          height: 48.0,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _flex1++;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: _flex2,
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: GestureDetector(
+                        child: Container(
+                          child: Center(
+                            child: Text(
+                              '$_flex2',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          margin: const EdgeInsets.all(10.0),
+                          color: Colors.green,
+                          width: 48.0,
+                          height: 48.0,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _flex2++;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+              Container(
+                child: RaisedButton(
+                  child: Text("Reset"),
+                  color: Colors.red,
+                  onPressed: () {
+                    setState(() {
+                      _flex0 = 1;
+                      _flex1 = 2;
+                      _flex2 = 1;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
